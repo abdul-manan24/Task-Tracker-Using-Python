a7693ca585af853fb78534f81617e9131bd70ff3
abdul-manan24 <manan.cs.2k24@gmail.com>
2026-08-28 19:17:34 +0500

    made functions

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 if __name__ == "__main__":
     class task_tracker:
-        pass
+
+        def add_task(task:str):
+            pass
+
+        def delete_task():
+            pass
+
+        def update_task():
+            pass
+
+        def list_all_tasks():
+            pass
+
+        def list_completed_tasks():
+            pass
+
+        def list_inprogress_tasks():
+            pass
+
+        def list_remaining_tasks():
+            pass
