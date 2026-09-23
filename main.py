157f108e50428ecaaabb21c14f0262148dbf4ce1
abdul-manan24 <manan.cs.2k24@gmail.com>
2026-09-23 19:24:28 +0500

    Pushing updated functions that now include time of creation and updation with task

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import os
 import json
+from datetime import datetime
  
 def add_task(task:str):
+    created_at = datetime.now()
+    created_at = created_at.strftime("%d-%m-%Y %I:%M %p")
     if not os.path.isfile("tasks.json"):
         with open("tasks.json", 'w'):
             pass
@@ -13,10 +16,19 @@ def add_task(task:str):
             task_information = json.load(file)
         task_id = len(task_information) + 1
 
-    task_information[task_id] = {"Description":task}
+    while str(task_id) in task_information.keys():
+        task_id += 1
 
+    # print(task_id in task_information.keys())
+    # print(type(task_id))
+
+    task_information[task_id] = {"Description":task, "Created at": created_at, "status":"Pending"}
+
+    # print(task_information)
     with open("tasks.json", 'w') as task_file:
         json.dump(task_information, task_file, indent=2)
+
+    print(f"Task added successfully!, assigned id {task_id}")
 
 def delete_task(task_id:int):
     if not os.path.isfile("tasks.json"):
@@ -27,26 +39,34 @@ def delete_task(task_id:int):
         task_information = json.load(file)
         try:
             del task_information[task_id]
+            with open("tasks.json", 'w') as file:
+                json.dump(task_information, file, indent=2)
+            
+            print("Task deleted successfully!")
         except KeyError:
             print("Key doesn't exist!")
 
-    with open("tasks.json", 'w') as file:
-        json.dump(task_information, file, indent=2)
-
 def update_task(task_id:int, task_description:str):
+    updated_at = datetime.now()
+    updated_at = updated_at.strftime("%d-%m-%Y %I:%M %p")
+
     if not os.path.isfile("tasks.json"):
         print("File does not exist, please add tasks first!")
         return
 
     with open("tasks.json", "r+") as file:
+    
         task_information = json.load(file)
         try:
-            task_information[task_id] = {"Description": task_description}
+            task_information[task_id]["Description"] = task_description
+            task_information[task_id]["Updated at"] = updated_at
         except KeyError:
             print("No any with that key exists!")
             
     with open("tasks.json", 'w') as file:
         json.dump(task_information, file, indent=2)
+
+    print("Task updated successfully!")
 
 def list_all_tasks():
     pass
