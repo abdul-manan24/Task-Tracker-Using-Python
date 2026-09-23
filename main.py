3fd757e6fb2c6fa700f8590c7fbbbd6ca05b0a8d
abdul-manan24 <manan.cs.2k24@gmail.com>
2026-09-23 22:11:53 +0500

    Pushing complete code of project without

--- main.py
+++ main.py
@@ -19,18 +19,15 @@ def add_task(task:str):
     while str(task_id) in task_information.keys():
         task_id += 1
 
-    # print(task_id in task_information.keys())
-    # print(type(task_id))
+    task_information[task_id] = {"Description":task, "Created at": created_at, "Status":"to-do"}
 
-    task_information[task_id] = {"Description":task, "Created at": created_at, "status":"Pending"}
-
-    # print(task_information)
     with open("tasks.json", 'w') as task_file:
-        json.dump(task_information, task_file, indent=2)
+        json.dump(task_information, task_file, indent=4)
 
     print(f"Task added successfully!, assigned id {task_id}")
 
-def delete_task(task_id:int):
+
+def delete_task(task_id):
     if not os.path.isfile("tasks.json"):
         print("File does not exist, please add tasks first!")
         return
@@ -40,13 +37,15 @@ def delete_task(task_id:int):
         try:
             del task_information[task_id]
             with open("tasks.json", 'w') as file:
-                json.dump(task_information, file, indent=2)
+                json.dump(task_information, file, indent=4)
             
             print("Task deleted successfully!")
         except KeyError:
             print("Key doesn't exist!")
 
-def update_task(task_id:int, task_description:str):
+
+def update_task(task_id, task_description:str):
+    
     updated_at = datetime.now()
     updated_at = updated_at.strftime("%d-%m-%Y %I:%M %p")
 
@@ -64,22 +63,95 @@ def update_task(task_id:int, task_description:str):
             print("No any with that key exists!")
             
     with open("tasks.json", 'w') as file:
-        json.dump(task_information, file, indent=2)
+        json.dump(task_information, file, indent=4)
 
     print("Task updated successfully!")
 
+
+def mark_task(task_id, status):
+    if not os.path.isfile("tasks.json"):
+        print("File does not exist, please add tasks first!")
+        return
+
+    with open("tasks.json", "r") as file:
+        task_information = json.load(file)
+
+    task_information[task_id]["Status"] = status
+
+    with open("tasks.json", "w") as file:
+        json.dump(task_information, file, indent=4)
+
+    print(f"Task marked as {status}")
+
+
 def list_all_tasks():
-    pass
+
+    if not os.path.isfile("tasks.json"):
+        print("File does not exist, please add tasks first!")
+        return
+    
+    with open("tasks.json", "r") as file:
+        task_information = json.load(file)
+
+    print("\n------Listing All Tasks------\n")
+    for key, value in task_information.items():
+        print(f"----Task ID {key}----")
+        print(f"'{value["Description"]}'")
+        print(f"Task status: {value["Status"]}, Created at: {value["Created at"]}, Updated at: {value.get("Updated at")}")
+        print()
+
 
 def list_completed_tasks():
-    pass
+    if not os.path.isfile("tasks.json"):
+        print("File does not exist, please add tasks first!")
+        return
 
-def list_inprogress_tasks():
-    pass
+    with open("tasks.json", "r") as file:
+        task_information = json.load(file)
+
+    print("\n------Listing All Completed Tasks------\n")
+    for key, value in task_information.items():
+        if value["Status"] == "done":
+            print(f"----Task ID {key}----")
+            print(f"'{value["Description"]}'")
+            print(f"Task status: {value["Status"]}, Created at: {value["Created at"]}, Updated at: {value.get("Updated at")}")
+            print()
+
+
+def list_in_progress_tasks():
+    if not os.path.isfile("tasks.json"):
+        print("File does not exist, please add tasks first!")
+        return
+
+    with open("tasks.json", "r") as file:
+            task_information = json.load(file)
+
+    print("\n------Listing All In-process Tasks------\n")
+    for key, value in task_information.items():
+        if value["Status"] == "in-progress":
+            print(f"----Task ID {key}----")
+            print(f"'{value["Description"]}'")
+            print(f"Task status: {value["Status"]}, Created at: {value["Created at"]}, Updated at: {value.get("Updated at")}")
+            print()
+
 
 def list_remaining_tasks():
-    pass
+    if not os.path.isfile("tasks.json"):
+        print("File does not exist, please add tasks first!")
+        return
 
+    with open("tasks.json", "r") as file:
+            task_information = json.load(file)
+            
+    print("\n------Listing All Remaining Tasks------\n")
+    for key, value in task_information.items():
+        if value["Status"] != "done":
+            print(f"----Task ID {key}----")
+            print(f"'{value["Description"]}'")
+            print(f"Task status: {value["Status"]}, Created at: {value["Created at"]}, Updated at: {value.get("Updated at")}")
+            print()
+
+            
 def take_input():
 
     while True:
@@ -92,29 +164,63 @@ def take_input():
             print("Please write any command in input bar or exit")
             continue
 
-        function_to_call, task_description = input_info
+        if len(input_info) > 1:
+            function_to_call, task_description = input_info
 
-        if function_to_call.lower() == "update":
-            task_info = task_description.split(" ", 1)
-            task_id, task_description = task_info
+            if function_to_call.lower() == "update":
+                task_info = task_description.split(" ", 1)
+                task_id, task_description = task_info
+        
+            if function_to_call.lower() == "delete":
+                task_id = task_description
 
-        elif function_to_call.lower() == "delete":
-            task_id = task_description
+            if function_to_call.lower() == "mark-in-progress":
+                function_to_call = "mark"
+                task_id = task_description
+                status = "in-progress"
 
-        task_description = task_description.strip("\"")
+            if function_to_call.lower() == "mark-done":
+                function_to_call = "mark"
+                task_id = task_description
+                status = "done"
+
+            if function_to_call.lower() == "mark-to-do":
+                function_to_call = "mark"
+                task_id = task_description
+                status = "to-do"
+        
+            task_description = task_description.strip("\"")
+        else:
+            function_to_call = "".join(input_info)
+
 
         match function_to_call:
             case "add":
                 add_task(task_description)
+
             case "delete":
                 delete_task(task_id)
+
             case "update":
                 update_task(task_id, task_description)
+
+            case "mark":
+                mark_task(task_id, status)
+
+            case "list":
+                list_all_tasks()
+
+            case "list-in-progress":
+                list_in_progress_tasks()
+
+            case "list-completed":
+                list_completed_tasks()
+
+            case "list-remaining":
+                list_remaining_tasks()
+
             case _:
                 "Task not added"
 
-
-take_input()
-
 if __name__ == "__main__":
-    pass
+    take_input()
